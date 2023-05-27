3fcf22b2a9127477ffa844d139efe00aa9479148
yun ok <odu3971@gmail.com>
2023-05-27 16:17:13 +0900

    php

--- not_payed.php
+++ not_payed.php
@@ -1,3 +1,24 @@
 <?php
-echo "hello world";
+    $conn = mysqli_connect(
+    'localhost:3306',
+    'root',
+    'lab1234!',
+    'test1');
+    $sql = "SELECT * FROM name WHERE patient_name = idname";
+    $result = mysqli_query($conn, $sql);
+    // var_dump($result->num_rows);
+    if ($result->num_rows > 0){
+        while( $jb_row = mysqli_fetch_array( $jb_result ) ) {
+            echo
+              '<p>'
+              . $jb_row[ 'idname' ]
+              . $jb_row[ 'test' ]
+              . '</p>'
+            ;
+          }
+    }
+    else{
+        
+    }
+    
 ?>
